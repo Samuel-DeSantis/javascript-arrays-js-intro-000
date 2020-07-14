267124e9b9bb4c30d4cf83deed042efff6af7f2b
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-14 03:52:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,10 +6,22 @@ function addElementToEndOfArray(array, element) {return [...array, element];}
 function removeElementFromBeginningOfArray(array) {return array.slice(1, array.length);}
 function removeElementFromEndOfArray(array) {return array.slice(0, array.length - 1);}
 
-function destructivelyAddElementToBeginningOfArray(array, element) {return array.unshift(element);}
-function destructivelyAddElementToEndOfArray(array, element) {return array.push(element);}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {return array.shift();}
-function destructivelyRemoveElementFromEndOfArray(array) {return array.pop();}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
 
 function accessElementInArray(array, element) {return array[element];}
