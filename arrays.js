6d8c4e45beaa7a367123165827fb8567ee61445a
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-14 03:28:40 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {return [element, ...array];}
+function addElementToEndOfArray(array, element) {return [...array, element];}
+
+function removeElementFromBeginningOfArray(array) {return array.slice(1, array.length);}
+function removeElementFromEndOfArray(array) {return array.slice(0, array.length - 1);}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {return array.unshift(element);}
+function destructivelyAddElementToEndOfArray(array, element) {return array.push(element);}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {return array.shift();}
+function destructivelyRemoveElementFromEndOfArray(array) {return array.pop();}
+
+function accessElementInArray(array, element) {return array[element];}
