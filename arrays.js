10ba260ceb54b32e0a18263d1d24516342572003
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-14 03:46:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,11 +6,10 @@ function addElementToEndOfArray(array, element) {return [...array, element];}
 function removeElementFromBeginningOfArray(array) {return array.slice(1, array.length);}
 function removeElementFromEndOfArray(array) {return array.slice(0, array.length - 1);}
 
-function destructivelyAddElementToBeginningOfArray(array, element) {return array.unshift(element);}
-function destructivelyAddElementToEndOfArray(array, element) {return array.push(element);}
+function destructivelyAddElementToBeginningOfArray(array, element) {return array.push(element);}
+function destructivelyAddElementToEndOfArray(array, element) {return array.unshift(element);}
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {return array.shift();}
-function destructivelyRemoveElementFromEndOfArray(array) {return array.pop();}
+function destructivelyRemoveElementFromBeginningOfArray(array) {return array.pop();}
+function destructivelyRemoveElementFromEndOfArray(array) {return array.shift();}
 
 function accessElementInArray(array, element) {return array[element];}
-console.log(destructivelyAddElementToBeginningOfArray(chocolateBars,"butterfinger"));
